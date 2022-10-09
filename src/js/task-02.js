ce710f47b61460ced105e089b6cf0b31e83cddbc
KychataOlena <106249466+KychataOlena@users.noreply.github.com>
2022-10-09 22:17:17 +0300

    зроблено 02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const names = ingredients.map((ingredient) => {
+
+const itemEl = document.createElement('li');
+itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+  return itemEl;
+})
+
+const navListEl = document.querySelector('#ingredients');
+navListEl.append(...names);
+
+console.log(names)
+
