5dcb634f738e7a9cc00e8c5bb9cc38a777d14c3e
KychataOlena <106249466+KychataOlena@users.noreply.github.com>
2022-10-13 22:18:06 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,9 +3,10 @@ const itemEl = document.querySelectorAll(".item");
 console.log(`Number of categories: ${itemEl.length}`);
 
 itemEl.forEach((item) => {
-    const titleRef = item.querySelector("h2");
+   
+    const titleRef = item.firstElementChild;
     console.log(`Category:  ${titleRef.textContent}`);
-    const elements = item.querySelectorAll('li').length;
-     console.log(`Elements:  ${elements}`);
+  
+     console.log(`Elements:  ${item.lastElementChild.children.length}`);
 });
 
